381a703f4f36c1f51f9d89609ea43602206ce768
abdulhadi240 <ah912425@gmail.com>
2024-08-24 14:46:18 +0500

    api

--- app/(defaults)/page.tsx
+++ app/(defaults)/page.tsx
@@ -1,25 +1,23 @@
-'use client'
-import ComponentsDashboardSales from '@/components/dashboard/components-dashboard-sales';
+'use client';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import { Metadata } from 'next';
-import React from 'react';
-import { useEffect, useState } from 'react'
-
-
+import ComponentsDashboardSales from '@/components/dashboard/components-dashboard-sales';
 
 const Sales = () => {
-    const [orders, setOrders] = useState([]);
+  const [orders, setOrders] = useState([]);
 
   useEffect(() => {
     const fetchOrders = async () => {
       try {
         const response = await axios.get('https://b519f5-ff.myshopify.com/admin/api/2024-07/orders.json', {
-            headers: {
-              'X-Shopify-Access-Token': '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',
-              'Content-Type': 'application/json'
-            }
-          });
-        console.log(response);
+          headers: {
+            'X-Shopify-Access-Token': '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',
+            'Content-Type': 'application/json',
+          },
+        });
+        setOrders(response.data.orders || []);
+        console.log(orders);
+        
       } catch (error) {
         console.error('Error fetching orders:', error);
       }
@@ -28,8 +26,7 @@ const Sales = () => {
     fetchOrders();
   }, []);
 
-    return <ComponentsDashboardSales />;
-
+  return <ComponentsDashboardSales />;
 };
 
 export default Sales;
