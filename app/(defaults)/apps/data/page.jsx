067ec2d15f898984ca0fb2359472f3137fd968bf
abdulhadi240 <ah912425@gmail.com>
2024-08-29 19:05:19 +0500

    testing

--- app/(defaults)/apps/data/page.jsx
+++ app/(defaults)/apps/data/page.jsx
@@ -2,8 +2,7 @@
 import React from 'react'
 import axios from 'axios';
 const page = async () => {
-    const data = await axios.get("https://dashboard-shopify-backend2.onrender.com/orders")
-    console.log(data);
+    
     
   return (
     <div>page</div>
--- app/(defaults)/page.jsx
+++ app/(defaults)/page.jsx
@@ -1,13 +1,38 @@
+'use client'
+import React, { useEffect, useState } from 'react';
 import ComponentsDashboardSales from '@/components/dashboard/components-dashboard-sales';
+import axios from 'axios';
 
+const Sales = () => {
+  const [salesData, setSalesData] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
-const Sales = async () => {
-    return (
-      <div>
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get("https://dashboard-shopify-backend2.onrender.com/orders");
+        setSalesData(response.data);
+        console.log(salesData);
         
-        <ComponentsDashboardSales />
-      </div>
-    );
+        setLoading(false);
+      } catch (err) {
+        setError(err);
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  if (loading) return <div>Loading...</div>;
+  if (error) return <div>Error: {error.message}</div>;
+
+  return (
+    <div>
+      <ComponentsDashboardSales  />
+    </div>
+  );
 };
 
 export default Sales;
